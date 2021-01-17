0ccdcdeec29391440fc8c4fd3a83985fcaf1ca9b
Matteo Gaffuri <matteo.gaffuri@fmeeducation.it>
2021-01-17 23:22:39 +0100

    Add shapes

---
+++ structs/shapes.go
@@ -0,0 +1,43 @@
+package structs
+
+import "math"
+
+type Shape interface {
+	Area() float64
+}
+
+type Triangle struct {
+	Base   float64
+	Height float64
+}
+
+func (t Triangle) Area() float64 {
+	return (t.Base * t.Height) * 0.5
+}
+
+// Circle represents a circle...
+type Circle struct {
+	Radius float64
+}
+
+func (c Circle) Area() float64 {
+	return math.Pi * c.Radius * c.Radius
+}
+
+// Rectangle has the dimensions of a rectangle.
+type Rectangle struct {
+	Width  float64
+	Height float64
+}
+
+func (r Rectangle) Area() float64 {
+	return r.Width * r.Height
+}
+
+func Perimeter(rectangle Rectangle) (perimeter float64) {
+	return 2 * (rectangle.Width + rectangle.Height)
+}
+
+func Area(rectangle Rectangle) (area float64) {
+	return rectangle.Width * rectangle.Height
+}
